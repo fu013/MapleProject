e49155d6734523b38ff9a668bd4aca1a2eb9c422
fu013 <fu017@naver.com>
2023-02-25 01:56:30 +0900

    초기 프록시 설정 및 서버 구동 완료

--- backend/src/main/java/com/seungchan/backend/controller/HomeController.java
+++ backend/src/main/java/com/seungchan/backend/controller/HomeController.java
@@ -10,6 +10,6 @@ import java.util.List;
 public class HomeController {
     @GetMapping("hello")
     public List<String> Hello(){
-        return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+        return Arrays.asList("7000");
     }
 }
