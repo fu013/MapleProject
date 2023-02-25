6bf1972f085910d091c48461553832d8515b74b5
root <root@ip-172-31-15-93.ec2.internal>
2023-02-25 08:06:02 +0000

    리액트 & 스프링 8080포트에서 순차 빌드 및 deploy.sh 쉘 스크립트 배포 명령문 주가

---
+++ deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/seungchan
+PROJECT_NAME=backend
+
+cd $REPOSITORY
+
+echo "> 깃허브 pull"
+
+git pull
+
+cd $REPOSITORY/$PROJECT_NAME/
+
+echo "> 프로젝트 빌드 시작"
+
+./gradlew build
+
+echo "> 디렉토리로 이동"
+
+cd $REPOSITORY
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+cd $REPOSITORY/$PROJECT_NAME/build/libs
+
+java -jar backend-0.0.1-SNAPSHOT.jar &
+
